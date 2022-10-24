02f39542992e5a984386fcebda58c2f609764957
aboo212031 <abubakrryzan@gmail.com>
2022-10-24 16:37:03 +0530

    Adding Printer Class

---
+++ Printer.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Printer {
+    public static void main(String[] args){
+//        for(int i = 1;i<100;i++){
+//            if(i%2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+
+//        int i =2;
+//        while(i<100){
+//            System.out.println(i);
+//            i+=2;
+//        }
+//
+//        String x ="abc";
+//        String y = x;
+//        y = new String("123");
+//        String z = x;
+//        y = x;
+//        z = "456";
+//
+//        System.out.println(x+y+z);
+
+        int[] lotteryNums = new int[6];
+        Random random = new Random();
+
+
+    }
+}
